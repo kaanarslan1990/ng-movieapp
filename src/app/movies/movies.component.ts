f6e7bdb80b57726956fcf4499b47ba5ef0aa4ea2
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-06-25 00:08:26 +0300

    event-ngModelChange-onInputChange

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -5,28 +5,34 @@ import { MovieRepository } from '../models/movie.repository';
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css']
+  styleUrls: ['./movies.component.css'],
 })
 export class MoviesComponent implements OnInit {
-
-  title= "Film List";
+  title = 'Film List';
   movies: Movie[];
+  FilteredMovies: Movie[];
   popularMovies: Movie[];
   movieRepository: MovieRepository;
   today = new Date();
 
-  filterText: string = "";
+  filterText: string = '';
 
   constructor() {
     this.movieRepository = new MovieRepository();
     this.movies = this.movieRepository.getMovies();
     this.popularMovies = this.movieRepository.getPopularMovies();
-   }
-
-  ngOnInit(): void {
+    this.FilteredMovies = this.movies;
   }
 
+  ngOnInit(): void {}
 
-  
-
+  onInputChange() {
+    this.FilteredMovies = this.filterText
+      ? this.movies.filter(
+          (m) =>
+            m.title.indexOf(this.filterText) !== -1 ||
+            m.description.indexOf(this.filterText) !== -1
+        )
+      : this.movies;
+  }
 }
--- src/app/pipes/movie-filter.pipe.ts
+++ src/app/pipes/movie-filter.pipe.ts
@@ -8,8 +8,7 @@ export class MovieFilterPipe implements PipeTransform {
   transform(movies: Movie[], filterText: string): Movie[] {
     filterText = filterText.toLowerCase();
 
-    console.log(filterText);
-    console.log(movies);
+   
 
     return filterText? movies.filter((m: Movie) => m.title.toLowerCase().indexOf(filterText) !== -1 || m.description.toLowerCase().indexOf(filterText) !== -1): movies;
 
