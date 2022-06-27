4cfc67f028cf4046d5e233ee234d2a5c22a1f92b
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-06-27 17:07:21 +0300

    vustom validators

--- src/app/movie-create/movie-create.component.ts
+++ src/app/movie-create/movie-create.component.ts
@@ -5,6 +5,7 @@ import { Category } from '../models/category';
 import { AlertifyService } from '../services/alertify.service';
 import { CategoryService } from '../services/category.service';
 import { MovieService } from '../services/movie.service';
+import { ImageValidator } from '../validators/image.validator';
 
 @Component({
   selector: 'app-movie-create',
@@ -34,12 +35,16 @@ export class MovieCreateComponent implements OnInit {
   movieForm = new FormGroup({
     title: new FormControl('', [Validators.required, Validators.minLength(5)]),
     description: new FormControl('', [Validators.required]),
-    imageUrl: new FormControl('', [Validators.required]),
+    imageUrl: new FormControl('', [Validators.required, ImageValidator.isValidExtension]),
     categoryId: new FormControl('', [Validators.required]),
   });
 
   get title () {
     return this.movieForm.get("title");
+  }
+
+  get imageUrl () {
+    return this.movieForm.get("imageUrl")
   }
 
 
@@ -54,24 +59,20 @@ export class MovieCreateComponent implements OnInit {
 
   createMovie() {
 
-    console.log(this.movieForm)
-    console.log(this.movieForm.value.title)
+    
 
-
-
-
-    //   const movie = {
-    //     id: 0,
-    //     title: this.model.title,
-    //     description: this.model.description,
-    //     imageUrl: this.model.imageUrl,
-    //     isPopular: false,
-    //     datePublished: new Date().getTime(),
-    //     categoryId: this.model.categoryId,
-    //   };
-    //   this.movieService.createMovie(movie).subscribe((data) => {
-    //     this.router.navigate(['/movies', data.id]);
-    //   });
+      const movie = {
+        id: 0,
+        title: this.movieForm.value.title,
+        description:this.movieForm.value.description,
+        imageUrl: this.movieForm.value.imageUrl,
+        isPopular: false,
+        datePublished: new Date().getTime(),
+        categoryId: Number(this.movieForm.value.categoryId),
+      };
+      this.movieService.createMovie(movie).subscribe((data) => {
+        this.router.navigate(['/movies', data.id]);
+      });
   }
   log(value: any) {
     console.log(value);
