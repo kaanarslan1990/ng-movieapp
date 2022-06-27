ca7ba2f959dde7ca0bec56768d52a05a30742164
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-06-27 15:24:18 +0300

    reactive forms

--- src/app/movie-create/movie-create.component.ts
+++ src/app/movie-create/movie-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, NgModule, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Category } from '../models/category';
 import { AlertifyService } from '../services/alertify.service';
@@ -31,43 +31,45 @@ export class MovieCreateComponent implements OnInit {
     });
   }
 
-  createMovie() {
-    const movie = {
-      id: 0,
-      title: this.model.title,
-      description: this.model.description,
-      imageUrl: this.model.imageUrl,
-      isPopular: false,
-      datePublished: new Date().getTime(),
-      categoryId: this.model.categoryId,
-    };
+  movieForm = new FormGroup({
+    title: new FormControl('film name'),
+    description: new FormControl('film description'),
+    imageUrl: new FormControl('1.jpeg'),
+    categoryId: new FormControl('2'),
+  });
 
-    this.movieService.createMovie(movie).subscribe((data) => {
-      this.router.navigate(['/movies', data.id]);
-    });
+
+  clearForm() {
+    this.movieForm.patchValue({ 
+      title:'',
+      description: '',
+      imageUrl: '',
+      categoryId: '',
+    })
+  }
+
+  createMovie() {
+
+    console.log(this.movieForm)
+    console.log(this.movieForm.value.title)
+
+
+
+
+    //   const movie = {
+    //     id: 0,
+    //     title: this.model.title,
+    //     description: this.model.description,
+    //     imageUrl: this.model.imageUrl,
+    //     isPopular: false,
+    //     datePublished: new Date().getTime(),
+    //     categoryId: this.model.categoryId,
+    //   };
+    //   this.movieService.createMovie(movie).subscribe((data) => {
+    //     this.router.navigate(['/movies', data.id]);
+    //   });
   }
   log(value: any) {
     console.log(value);
   }
 }
-
-// title: any, description: any, imageUrl: any, categoryId: any
-// if (title.value === '' || description.value === '' || imageUrl === "" || categoryId.value === "-1") {
-//   this.alertify.error("You must fill in all fields ");
-//   return;
-// }
-
-// if (title.value.length < 2) {
-//   this.alertify.error("Title must be at least 2 characters!")
-//   return;
-// }
-// if (description.value.length < 10 || description.value.length > 50) {
-//   this.alertify.error("Description must be between 10 and 50 characters!")
-//   return;
-// }
-
-// const extensions = ["jpeg", "png", "jpg", "gif"];
-//     const extension = imageUrl.value.split('.').pop();
-//     if (extensions.indexOf(extension) === -1) {
-//       this.alertify.error("You can only add 'jpeg', 'png', 'jpg', 'gif' format!");
-//       return;
