e7656df57bd45c4e0e45e2d96359def83189a8b2
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-06-27 12:22:16 +0300

    add film to db

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,6 +1,10 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {  Observable,throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Movie } from '../models/movie';
 
@@ -11,50 +15,58 @@ export class MovieService {
   constructor(private http: HttpClient) {}
 
   getMovies(categoryId: number): Observable<Movie[]> {
-
     let newUrl = this.url;
 
-    if(categoryId) {
-      newUrl += '?categoryId=' +categoryId;
-
+    if (categoryId) {
+      newUrl += '?categoryId=' + categoryId;
     }
 
-    return this.http
-      .get<Movie[]>(newUrl)
-      .pipe(
-        tap((data) => console.log(data)),
-        catchError(this.handleError)
-        );
-  }
-
-  getMovieById(movieId:number): Observable<Movie> {
-    return this.http.get<Movie>(this.url + "/" + movieId).pipe(
+    return this.http.get<Movie[]>(newUrl).pipe(
       tap((data) => console.log(data)),
       catchError(this.handleError)
-      );
+    );
+  }
+
+  getMovieById(movieId: number): Observable<Movie> {
+    return this.http.get<Movie>(this.url + '/' + movieId).pipe(
+      tap((data) => console.log(data)),
+      catchError(this.handleError)
+    );
+  }
+
+  createMovie(movie: Movie): Observable<Movie> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Token',
+      }),
+    };
+    return this.http.post<Movie>(this.url, movie, httpOptions).pipe(
+      tap((data) => console.log(data)),
+      catchError(this.handleError)
+    );
   }
 
   private handleError(error: HttpErrorResponse) {
-    if(error.error instanceof ErrorEvent) {
-        //client yada network
-        console.log("error: " + error.error.message)
+    if (error.error instanceof ErrorEvent) {
+      //client yada network
+      console.log('error: ' + error.error.message);
     } else {
-        //backend
-        switch(error.status) {
-            case 404:
-                console.log("Not found");
-                break;
-            case 403:
-                console.log("Access denied");
-                break;
-            case 501:
-                console.log("Interval error");
-                break;
-            default:
-                console.log("Unknown error");
-
-        }
+      //backend
+      switch (error.status) {
+        case 404:
+          console.log('Not found');
+          break;
+        case 403:
+          console.log('Access denied');
+          break;
+        case 501:
+          console.log('Interval error');
+          break;
+        default:
+          console.log('Unknown error');
+      }
     }
-    return throwError(()=>"Error");
+    return throwError(() => 'Error');
   }
 }
