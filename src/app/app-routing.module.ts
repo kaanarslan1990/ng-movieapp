dadcc088fda3af3deb95915e598f0137a447b8f0
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-06-29 10:24:46 +0300

    add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { CategoryCreateComponent } from './category-create/category-create.component';
+import { AuthGuard } from './guards/auth.guards';
 import { MovieCreateComponent } from './movie-create/movie-create.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { MoviesComponent } from './movies/movies.component';
 
 const routes: Routes = [
-  { path: 'movies', component: MoviesComponent },
+  { path: 'movies', component: MoviesComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'movies', pathMatch: 'full' },
-  { path: 'movies/category/:categoryId', component: MoviesComponent},
-  { path: 'movies/create', component: MovieCreateComponent},
-  { path: 'categories/create', component: CategoryCreateComponent},
-  { path: 'movies/:movieId', component: MovieDetailsComponent},
+  { path: 'movies/category/:categoryId', component: MoviesComponent, canActivate: [AuthGuard] },
+  { path: 'movies/create', component: MovieCreateComponent, canActivate: [AuthGuard]},
+  { path: 'categories/create', component: CategoryCreateComponent, canActivate: [AuthGuard]},
+  { path: 'movies/:movieId', component: MovieDetailsComponent, canActivate: [AuthGuard]},
   { path: 'auth', component: AuthComponent},
 ];
 
