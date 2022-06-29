61edfd9a1af2ac15a29d542681c8cdad3af50cce
Kaan Arslan <oguzkaanarslan@outlook.com>
2022-06-29 10:12:21 +0300

    auto login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,67 +5,89 @@ import { BehaviorSubject, tap } from 'rxjs';
 import { AuthResponse } from '../models/AuthResponse';
 import { User } from '../models/user';
 
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  api_key ="AIzaSyBqjn1TUeOvNQ5ugGNrV_IX64QEMfWepps"
+  api_key = 'AIzaSyBqjn1TUeOvNQ5ugGNrV_IX64QEMfWepps';
 
   user = new BehaviorSubject<User>(null);
 
-  constructor(private http: HttpClient, private router: Router) { }
-
+  constructor(private http: HttpClient, private router: Router) {}
 
   signUp(email: string, password: string) {
-    return this.http.post<AuthResponse>("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + this.api_key, {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    }).pipe(
-      tap(response => {
-        const expirationDate =new Date(new Date().getTime() + (+response.expiresIn *1000))
-
-        const user = new User(
-          response.email,
-          response.localId,
-          response.idToken,
-          expirationDate
-        );
-
-        this.user.next(user);
-      })
-    )
+    return this.http
+      .post<AuthResponse>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+          this.api_key,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        tap((response) => {
+          this.handleAuthentication(response.email, response.localId, response.idToken, +response.expiresIn )
+        })
+      );
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponse>("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + this.api_key, {
-      email: email,
-      password: password,
-      returnSecureToken: true
-    }).pipe(
-      tap(response => {
-        const expirationDate =new Date(new Date().getTime() + (+response.expiresIn *1000))
-
-        const user = new User(
-          response.email,
-          response.localId,
-          response.idToken,
-          expirationDate
-        );
-
-        this.user.next(user);
-      })
-    )
+    return this.http
+      .post<AuthResponse>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
+          this.api_key,
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(tap((response) => {
+        this.handleAuthentication(response.email, response.localId, response.idToken, +response.expiresIn )
+      }));
   }
 
   logout() {
     this.user.next(null);
-    this.router.navigate(['/auth'])
+    localStorage.removeItem('user')
+    this.router.navigate(['/auth']);
   }
-  
+
+  autoLogin() {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if(!user) {
+      return;
+    }
+
+    const loadedUser = new User(
+      user.email,
+      user.id,
+      user._token,
+      new Date(user._tokenExpirationDate)
+    );
+
+    if(loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
+  handleAuthentication(email: string, userId: string, token: string, expiresIn:number) {
+    const expirationDate = new Date(
+      new Date().getTime() + (expiresIn * 1000)
+    );
+
+    const user = new User(
+     email,
+     userId,
+     token,
+     expirationDate
+    );
+
+    this.user.next(user);
+
+    localStorage.setItem("user", JSON.stringify(user));
+  }
 }
-
-
